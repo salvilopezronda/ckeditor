749c08d09add0c42981bdd935b78bea69ca3e5af
salvilopezronda <salvilopezronda@gmail.com>
2023-05-10 09:54:43 +0200

    [03] Add 3 botones personalizables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component,ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CKEditorComponent } from 'ng2-ckeditor';
 @Component({
   selector: 'app-root',
@@ -7,44 +7,72 @@ import { CKEditorComponent } from 'ng2-ckeditor';
 })
 export class AppComponent {
   title = 'ckeditor';
-  ckeditorContent:string= '';
-  @ViewChild(CKEditorComponent) ckeditor:CKEditorComponent;
+  ckeditorContent: string = '';
+  @ViewChild(CKEditorComponent) ckeditor: CKEditorComponent;
 
-  ngAfterViewChecked(){
-    let editor =  this.ckeditor.instance;
-    editor.addCommand('yourCustomCommand', {
-      // @ts-ignore
-      exec: () => {
-        alert("boton nuevo")
-      },
-      refresh: () => {},
-    });
-
-
+  ngAfterViewChecked() {
+    let editor = this.ckeditor.instance;
     editor.config.height = '400';
-  	editor.config.toolbarGroups = [
-      { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-      { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-      { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-      { name: 'forms', groups: [ 'forms' ] },
+    editor.config.toolbarGroups = [
+      { name: 'document', groups: ['mode', 'document', 'doctools'] },
+      { name: 'clipboard', groups: ['clipboard', 'undo'] },
+      { name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing'] },
+      { name: 'forms', groups: ['forms'] },
       '/',
-      { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-      { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
-      { name: 'links', groups: [ 'links' ] },
-      { name: 'insert', groups: [ 'insert' ] },
+      { name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
+      { name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi', 'paragraph'] },
+      { name: 'links', groups: ['links'] },
+      { name: 'insert', groups: ['insert'] },
       '/',
-      { name: 'styles', groups: [ 'styles' ] },
-      { name: 'colors', groups: [ 'colors' ] },
-      { name: 'tools', groups: [ 'tools' ] },
-      { name: 'others', groups: [ 'others'] },
+      { name: 'styles', groups: ['styles'] },
+      { name: 'colors', groups: ['colors'] },
+      { name: 'tools', groups: ['tools'] },
+      { name: 'others', groups: ['others', 'boton1', 'boton2', 'boton3'] },
     ];
     editor.config.removeButtons = 'Preview,PasteFromWord,Anchor,about';
-    
 
 
+//boton1
+    editor.ui.addButton('boton1', {
+      label: 'comandoBoton1',
+      command: 'comandoBoton1',
+      toolbar: 'others',
+      icon: 'https://img2.freepng.es/20180901/qoa/kisspng-computer-icons-hyperlink-favicon-url-shortening-po-ms-lianjie-svg-png-icon-free-download-331745-5b8ad9b3d25566.5256320315358263558615.jpg'
+    });
 
-  
+    editor.addCommand('comandoBoton1', {
+      //@ts-ignore
+      exec: function (editor) {
+        alert('boton1');
+      }
+    });
+//boton2
+    editor.ui.addButton('boton2', {
+      label: 'comandoBoton2',
+      command: 'comandoBoton2',
+      toolbar: 'others',
+      icon: 'https://img2.freepng.es/20180901/qoa/kisspng-computer-icons-hyperlink-favicon-url-shortening-po-ms-lianjie-svg-png-icon-free-download-331745-5b8ad9b3d25566.5256320315358263558615.jpg'
+    });
+
+    editor.addCommand('comandoBoton2', {
+      //@ts-ignore
+      exec: function (editor) {
+        alert('boton2');
+      }
+    });
+
+    editor.ui.addButton('boton3', {
+      label: 'comandoBoton3',
+      command: 'comandoBoton3',
+      toolbar: 'others',
+      icon: 'https://img2.freepng.es/20180901/qoa/kisspng-computer-icons-hyperlink-favicon-url-shortening-po-ms-lianjie-svg-png-icon-free-download-331745-5b8ad9b3d25566.5256320315358263558615.jpg'
+    });
+
+    editor.addCommand('comandoBoton3', {
+      //@ts-ignore
+      exec: function (editor) {
+        alert('boton3');
+      }
+    });
   }
-  }
-
-
+}
